959cea280879c9335a90f9225211d7e1c97c13d8
Yhantik <yhantik@ya.ru>
2024-07-01 04:29:18 +0400

    feat: 1.0 added models, controllers, exception

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,79 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.LocalDate;
+import java.util.*;
 
 @RestController
+@RequestMapping("/films")
+@Slf4j
 public class FilmController {
+
+    private final Map<Long, Film> films = new HashMap<>();
+
+    @GetMapping
+    public Collection<Film> getFilms() {
+        log.info("Получение всех фильмов");
+        return films.values();
+    }
+
+    @PostMapping
+    public Film addFilm(@RequestBody Film film) {
+        validateFilm(film);
+
+        film.setId(getNextId());
+        films.put(film.getId(), film);
+        log.info("Фильм добавлен: {}", film);
+        return film;
+    }
+
+    @PutMapping
+    public Film updateFilm(@RequestBody Film film) {
+        validateFilm(film);
+
+        if (!films.containsKey(film.getId())) {
+            String message = "Фильм не найден";
+            log.error(message);
+            throw new ValidationException(message);
+        }
+        films.put(film.getId(), film);
+        log.info("Фильм обновлен: {}", film);
+        return film;
+    }
+
+    private void validateFilm(Film film) {
+        if (film.getName() == null || film.getName().isEmpty()) {
+            String message = "Название не должно быть пустым";
+            log.error(message);
+            throw new ValidationException(message);
+        }
+        if (film.getDescription() != null && film.getDescription().length() > 200) {
+            String message = "Максимальная длина описания должна быть не больше 200 символов";
+            log.error(message);
+            throw new ValidationException(message);
+        }
+        if (film.getReleaseDate() == null || film.getReleaseDate().isBefore(LocalDate.of(1895, 12, 28))) {
+            String message = "Дата релиза не должна быть раньше 28.12.1895";
+            log.error(message);
+            throw new ValidationException(message);
+        }
+        if (film.getDuration() == null || film.getDuration() <= 0) {
+            String message = "Продолжительность фильма должна быть положительным числом";
+            log.error(message);
+            throw new ValidationException(message);
+        }
+    }
+
+    private long getNextId() {
+        long currentMaxId = films.keySet()
+                .stream()
+                .mapToLong(id -> id)
+                .max()
+                .orElse(0);
+        return ++currentMaxId;
+    }
 }
