12b18411317ea8680b80ce2111d653b490de68ab
Yhantik <yhantik@ya.ru>
2024-07-03 21:16:19 +0400

    refactor: added builder

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,13 +1,13 @@
 package ru.yandex.practicum.filmorate.model;
 
 import jakarta.validation.constraints.*;
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
+@Builder
 public class Film {
     private Long id;
 
